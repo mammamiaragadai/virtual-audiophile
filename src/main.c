a5a86d9aa5c92438fdde84f0b1628f82afbfff87
Jay <jayay98@gmail.com>
2024-11-26 21:36:44 +0800

    Add matio

--- src/main.c
+++ src/main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <matio.h>
+
+void LoadMat(const char* path);
 
 int main(void)
 {
+    LoadMat("Dynaudio_Xeo_3.mat");
     puts("Hello world!");
     return 0;
+}
+
+void LoadMat(const char* path)
+{
+    mat_t* matfp;
+    matvar_t* matvar;
+
+    matfp = Mat_Open(path, MAT_ACC_RDONLY);
+    if (matfp == NULL)
+    {
+        printf("Error opening mat file: %s\n!", path);
+    }
+
+    const char* header = Mat_GetHeader(matfp);
+    printf("Header: %s\n", header);
+
+    matvar = Mat_VarRead(matfp, "irs");
+    if (matvar != NULL)
+    {
+        Mat_VarPrint(matvar, 1);
+        Mat_VarFree(matvar);
+    }
+
+    Mat_Close(matfp);
 }
