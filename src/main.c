a7d0906b9e2369b5e7ae94d22a6d337a0bab91a7
AYHH <hhj.auyeung@synovate.tech>
2024-11-27 15:03:06 +0800

    Reads scalars

--- src/main.c
+++ src/main.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <matio.h>
 
-void LoadMat(const char* path);
+typedef struct {
+    double  R; // scalar
+    double* azimuth; // vector
+    double* colatitude; // vector
+    double  fs; // scalar
+    double* irs; // matrix
+    double order; // scalar
+} directivity_t;
+
+int load_mat(const char* path, directivity_t *directivity);
 
 int main(void)
 {
-    LoadMat("Dynaudio_Xeo_3.mat");
     puts("Hello world!");
+
+    directivity_t* directivity = (directivity_t*) malloc(sizeof(directivity_t));
+    if (0 != load_mat("Dynaudio_Xeo_3.mat", directivity)) {
+        return -1;
+    }
+
+    printf("R: %g | fs: %g | order: %g\n", directivity->R, directivity->fs, directivity->order);
     return 0;
 }
 
-void LoadMat(const char* path)
+int load_mat(const char* path, directivity_t *directivity)
 {
     mat_t* matfp;
     matvar_t* matvar;
@@ -19,15 +34,16 @@ void LoadMat(const char* path)
     if (matfp == NULL)
     {
         printf("Error opening mat file: %s\n!", path);
+        return -1;
     }
 
-    const char* header = Mat_GetHeader(matfp);
-    printf("Header: %s\n", header);
-
-    while ( NULL != (matvar = Mat_VarReadNext(matfp)) ) {
-        Mat_VarPrint(matvar, 1);
-        Mat_VarFree(matvar);
-    }
+    matvar = Mat_VarRead(matfp, "R");
+    directivity->R = *(double*) matvar->data;
+    matvar = Mat_VarRead(matfp, "fs");
+    directivity->fs = *(double*) matvar->data;
+    matvar = Mat_VarRead(matfp, "order");
+    directivity->order = *(double*) matvar->data;
 
     Mat_Close(matfp);
+    return 0;
 }
