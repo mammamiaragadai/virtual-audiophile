b3f2588e577220fb7109dde43ddb2df8d449c08b
AYHH <hhj.auyeung@synovate.tech>
2024-11-27 15:32:41 +0800

    Import array type data; With simple gc

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <matio.h>
+#include <string.h>
 
 typedef struct {
     double  R; // scalar
@@ -8,32 +9,43 @@ typedef struct {
     double  fs; // scalar
     double* irs; // matrix
     double order; // scalar
+
+    unsigned int n_recievers; // scalar
 } directivity_t;
 
-int load_mat(const char* path, directivity_t *directivity);
+int load_directivity(const char* matpath, directivity_t *directivity);
+void free_directivity(directivity_t* directivity);
+void print_array(double* array, int n);
 
 int main(void)
 {
     puts("Hello world!");
 
+    // Initializations
     directivity_t* directivity = (directivity_t*) malloc(sizeof(directivity_t));
-    if (0 != load_mat("Dynaudio_Xeo_3.mat", directivity)) {
+    if (0 != load_directivity("Dynaudio_Xeo_3.mat", directivity)) {
         return -1;
     }
 
+    // Processing
     printf("R: %g | fs: %g | order: %g\n", directivity->R, directivity->fs, directivity->order);
+    print_array(directivity->azimuth, directivity->n_recievers);
+    print_array(directivity->colatitude, directivity->n_recievers);
+
+    // Destroy
+    free_directivity(directivity);
     return 0;
 }
 
-int load_mat(const char* path, directivity_t *directivity)
+int load_directivity(const char* matpath, directivity_t *directivity)
 {
     mat_t* matfp;
     matvar_t* matvar;
 
-    matfp = Mat_Open(path, MAT_ACC_RDONLY);
+    matfp = Mat_Open(matpath, MAT_ACC_RDONLY);
     if (matfp == NULL)
     {
-        printf("Error opening mat file: %s\n!", path);
+        printf("Error opening mat file: %s\n!", matpath);
         return -1;
     }
 
@@ -43,7 +55,32 @@ int load_mat(const char* path, directivity_t *directivity)
     directivity->fs = *(double*) matvar->data;
     matvar = Mat_VarRead(matfp, "order");
     directivity->order = *(double*) matvar->data;
+    matvar = Mat_VarRead(matfp, "azimuth");
+    directivity->azimuth = (double*) malloc(matvar->nbytes);
+    memcpy(directivity->azimuth, matvar->data, matvar->nbytes);
+    matvar = Mat_VarRead(matfp, "colatitude");
+    directivity->colatitude = (double*) malloc(matvar->nbytes);
+    memcpy(directivity->colatitude, matvar->data, matvar->nbytes);
+    directivity->n_recievers = matvar->nbytes / matvar->data_size;
 
+    Mat_VarFree(matvar);
     Mat_Close(matfp);
     return 0;
+}
+
+void free_directivity(directivity_t* directivity)
+{
+    free(directivity->azimuth);
+    free(directivity->colatitude);
+    free(directivity);
+}
+
+void print_array(double* array, int n)
+{
+    printf("[");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%f ", array[i]);
+    }
+    printf("]\n");
 }
